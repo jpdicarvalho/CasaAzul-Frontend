48f8b1c48a0a7cd8536ec6a2fa7ac3dd31e72676
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-05-13 20:51:23 -0300

    start menu lateral

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -1,8 +1,25 @@
+import './HomePage.css'
+import logoCasaAzul from './logo-casaAzul.png'
+
 const HomePage = () =>{
 
     return(
-        <div>
-            Olá mundo!
+        <div className="menu__lateral">
+            <div className="logo">
+                CASA AZUL
+            </div>
+            <div className="container__img__logo">
+                <div className="container__image__logo">
+                    <img src={logoCasaAzul} alt="logo da casa azul" className='img'/>
+                </div>
+                <div className="name__user">
+                    João Pedro
+                </div>
+                <div className="name__type__user">
+                    Adimistrador
+                </div>
+            </div>
+                
         </div>
     )
 };
